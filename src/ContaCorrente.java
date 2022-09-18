2e1132c561324d6fb307103d6a5647735f74452a
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2022-09-18 13:48:23 -0300

    Desafio atualizando

---
+++ src/ContaCorrente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NevesRibeiro
+ */
+public class ContaCorrente {
+    public int agencia;
+    public int numero;
+    public double saldo;
+    
+    public void sacar (){
+    
+}
+    public void depositar(){
+        
+    }
+    Public void transferir(){
+        
+    }
---
+++ src/Poupanca.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NevesRibeiro
+ */
+public class Poupanca {
+    public int agencia;
+    public int numero;
+    public double saldo;
+    public void sacar (){
+        
+    }
+    public void depositar (){
+        
+    }
+    public void transferir(){
+        
+    }
+}
+
