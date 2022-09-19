677b2fc7ccdb027eb73ac9aaf8cd8c1690df2971
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2022-09-18 22:37:03 -0300

    atualizando

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -8,8 +8,13 @@
  * @author NevesRibeiro
  */
 public class ContaCorrente extends Conta {
+    
+    private static int SEQUENCIAL = 1;
 
     public ContaCorrente() {
+        
+        super.agencia = 1;
+        super.numero = SEQUENCIAL;
     }
    
 
